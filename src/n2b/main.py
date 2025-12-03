671e0d8612ceda53edccfe0f5be3d86280192d20
dasu.archive <dasu.archive@gmail.com>
2025-12-03 17:22:07 +0900

    핵심기능 테스트 완료

--- src/n2b/main.py
+++ src/n2b/main.py
@@ -54,30 +54,30 @@ def run():
 
     # 2. 페이지 리스트 가져오기
     pages = notion_bot.get_preprocessed_notion_pages_info(page_size=15)
+    
+    with TistoryBot() as tistory_bot:
+        tistory_bot.do_login()
+        for page in pages:
 
-    for page in pages:
+            # 3. 페이지의 매핑 확인
+            attribute_mapping = notion_bot.update_attribute_mappings_by_id(category_id=page["category_id"], group_id=page["group_id"])
+            
+            # 4. tistory 에 카테고리 확인 및 생성 요청
+            if attribute_mapping.tistory_id is None or attribute_mapping.tistory_id == 0:
+                tistory_bot.update_category(attribute_mapping)
+            
+            # 5. 페이지 to markdown 변환 및 포스팅
+            content = notion_bot.get_page_to_html(page_id=page["id"])
+            
 
-        # 3. 페이지의 매핑 확인
-        attribute_mapping = notion_bot.update_attribute_mappings_by_id(category_id=page["category_id"], group_id=page["group_id"])
-        
-        # 4. tistory 에 카테고리 확인 및 생성 요청
-        if attribute_mapping.tistory_id is None or attribute_mapping.tistory_id == 0:
-            tistory_bot.update_category(attribute_mapping)
-        
-        # 5. 페이지 to markdown 변환 및 포스팅
-        markdown_content = notion_bot.get_page_to_html(page_id=page["id"])
-        
-
-        # 6. tistory 에 md 업로드
-        tistory_bot.upload_post(title=page["title"], content=markdown_content, tag=page["tags"], category_id=attribute_mapping.tistory_id)
-        
-        # 7 notion 에 업로드 처리
-        notion_bot.update_publication_date(page["id"])
+            # 6. tistory 에 md 업로드
+            tistory_bot.upload_post(title=page["title"], content=content, tag=page["tags"], category_id=attribute_mapping.tistory_id)
+            
+            # 7 notion 에 업로드 처리
+            notion_bot.update_publication_date(page["id"])
 
 if __name__ == "__main__":
-    # test_notion_attribute_update()
-    test_tistory_category_update()
-    # test_notion_page_to_html()
+    run()
         
     
 
